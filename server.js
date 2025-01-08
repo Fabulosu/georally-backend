1203540e21da81992c6ab6fa0ee4f9345f24e627
Edward <batalanedward@gmail.com>
2025-01-08 15:13:48 +0200

    added devmode and production deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connectDB = require('./src/configs/database');
 
 const authRoutes = require('./src/routes/authRoutes');
 
-const { PORT } = require('./src/configs');
+const { PORT, DEVMODE } = require('./src/configs');
 const socketHandler = require('./src/services/socketService');
 
 const app = express();
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: DEVMODE ? '*' : 'https://georally.vercel.app',
         methods: ['GET', 'POST'],
     },
 });
@@ -25,23 +25,30 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 
 connectDB().then(() => {
-    server.listen(PORT, '192.168.0.107', () => {
-        const os = require('os');
-        const networkInterfaces = os.networkInterfaces();
+    if (DEVMODE) {
+        server.listen(PORT, '192.168.0.107', () => {
+            const os = require('os');
+            const networkInterfaces = os.networkInterfaces();
 
-        const localIP = Object.values(networkInterfaces)
-            .flat()
-            .find(
-                (iface) =>
-                    iface.family === 'IPv4' &&
-                    !iface.internal &&
-                    iface.address.startsWith('192.168.')
-            )?.address || 'localhost';
+            const localIP = Object.values(networkInterfaces)
+                .flat()
+                .find(
+                    (iface) =>
+                        iface.family === 'IPv4' &&
+                        !iface.internal &&
+                        iface.address.startsWith('192.168.')
+                )?.address || 'localhost';
 
-        console.log(`Server is running on:
-        - Local: http://localhost:${PORT}
-        - Network: http://${localIP}:${PORT}`);
-    });
+            console.log(`Server is running on:
+            - Local: http://localhost:${PORT}
+            - Network: http://${localIP}:${PORT}`);
+        });
+    } else {
+        server.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }
+
 });
 
 socketHandler(io);
--- src/configs/index.js
+++ src/configs/index.js
@@ -3,4 +3,5 @@ require('dotenv').config();
 module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
     PORT: process.env.PORT || 3000,
+    DEVMODE: process.env.DEVMODE
 };
